d95b7815956a55b6b3508466d7816ecb7e568bda
Oliver Zimpasser <mail@olikurt.de>
2019-08-26 22:57:53 +0200

    make fake-backend async

--- fake-backend/src/main/java/de/oglimmer/async/fakebackend/service/SlowResourceService.java
+++ fake-backend/src/main/java/de/oglimmer/async/fakebackend/service/SlowResourceService.java
@@ -1,21 +1,31 @@
 package de.oglimmer.async.fakebackend.service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 public class SlowResourceService {
 
+	private final ExecutorService service = Executors.newFixedThreadPool(50);
+
 	@GetMapping(path = "/queryResource")
-	public String query() {
-		try {
-			TimeUnit.SECONDS.sleep(5);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-		return "<backend-data>";
+	public DeferredResult<ResponseEntity<String>> query() {
+		DeferredResult<ResponseEntity<String>> result = new DeferredResult<>();
+		service.submit(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+			result.setResult(ResponseEntity.ok("<backend-data>"));
+		});
+		return result;
 	}
 
 }
